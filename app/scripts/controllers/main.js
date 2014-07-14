5d06987260f8dd8b34d59c62df23c90f2ca88946
Sergey Bolshchikov <sergey@bolshchikov.net>
2014-07-14 13:30:42 +0300

    tile refactored

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,7 +3,7 @@
 angular.module('ngMinesweeperAppInternal')
   .controller('MainCtrl', function (Grid) {
     this.generate = function (width, height, amount) {
-      this.grid = new Grid(parseInt(width, 10), parseInt(height, 10), parseInt(amount, 10));
+      this.grid = new Grid(width, height, amount);
     };
   });
 
--- app/scripts/directives/tile.js
+++ app/scripts/directives/tile.js
@@ -1,15 +1,16 @@
 'use strict';
 
 angular.module('ngMinesweeperAppInternal')
-  .directive('tile', function (tileState) {
+  .directive('tile', function () {
     return {
-//      templateUrl: 'views/tile.html',
-      template:  '<div class="tile" ng-class="{discovered: isDiscovered(), marked: isMarked(), mined: isMined()}" ng-click="reveal()" right-click="toggle()"><span ng-if="isDiscovered()">{{value ? value : \'\'}}</span></div>',
+      templateUrl: 'views/tile.html',
+//      template:  '<div class="tile" ng-class="{discovered: isDiscovered(), marked: isMarked(), mined: isMined()}" ng-click="reveal()" right-click="toggle()"><span ng-if="isDiscovered()">{{value ? value : \'\'}}</span></div>',
       restrict: 'E',
+      replace: true,
       scope: {
         x: '@',
         y: '@',
-        value: '=',
+        tile: '=',
         grid: '='
       },
 
@@ -19,36 +20,23 @@ angular.module('ngMinesweeperAppInternal')
         var y = parseInt(scope.y, 10);
 
         scope.isDiscovered = function () {
-          return scope.value >= 0;
+          return scope.tile.value >= 0;
         };
 
         scope.isMarked = function () {
-          return scope.value === tileState.MARKED;
+          return scope.tile.marked && !scope.isDiscovered();
         };
 
         scope.isMined = function () {
-          return scope.value === tileState.MINE;
+          return scope.tile.mine && scope.isDiscovered();
         };
 
         scope.reveal = function () {
-          if (scope.grid.board[x][y] >= 0) {
-            return;
-          }
-          var res = scope.grid.reveal(x, y);
-          if (res === 0) {
-            scope.grid.revealMore(x, y);
-          }
+          scope.grid.reveal(x, y);
         };
 
         scope.toggle = function () {
-          if (scope.grid.board[x][y] === tileState.UNKNOWN) {
-            scope.grid.mark(x, y);
-            return;
-          }
-          if (scope.grid.board[x][y] === tileState.MARKED) {
-            scope.grid.unmark(x, y);
-            return;
-          }
+          scope.grid.toggle(x, y);
         };
       }
     };
