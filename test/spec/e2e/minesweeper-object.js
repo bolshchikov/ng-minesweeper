1b42dffbc9f86199888d110b357ebe381ed76e6c
Sergey Bolshchikov <sergey@bolshchikov.net>
2014-07-14 09:09:39 +0300

    e2e update

--- test/spec/e2e/minesweeper-object.js
+++ test/spec/e2e/minesweeper-object.js
@@ -4,18 +4,30 @@ function MineSweeperObject() {
 
   this.container = element(by.css('.grid-wrapper'));
   this.remains = element(by.css('.status-bar input'));
+  this.rows = element.all(by.repeater('row in main.grid.board'));
+  this.tiles = element.all(by.css('tile'));
+
+  this._getTiles = function (row) {
+    return row.all(by.repeater('tile in row'));
+  };
+
+  this._getTile = function (x, y) {
+    return this._getTiles(this.rows.get(x)).get(y).element(by.css('tile'));
+  };
 
   this.revealTile = function (x, y) {
-
+    var tile = this._getTile(x, y);
+    tile.click();
+    return tile;
   };
 
   this.markTile = function (x, y) {
-
+    var tile = this._getTile(x, y);
+    browser.actions().mouseMove(tile).perform();
+    browser.actions().click(protractor.Button.RIGHT).perform();
+    return tile;
   };
 
-  this.unmarkTile = function (x, y) {
-
-  };
 
 }
 
--- test/spec/e2e/scenarios.js
+++ test/spec/e2e/scenarios.js
@@ -33,4 +33,34 @@ describe('ngMinesweeperApp', function () {
     expect(grid.remains.getAttribute('value')).toEqual('10');
   });
 
+  it('should show the correct grid', function () {
+    page.get();
+    page.setParams(9, 9, 10);
+    var grid = page.generateGrid();
+    expect(grid.rows.count()).toEqual(9);
+    expect(grid.tiles.count()).toEqual(81);
+  });
+
+  it('should reveal a tile', function () {
+    page.get();
+    page.setParams(9, 9, 10);
+    var grid = page.generateGrid();
+    var tile = grid.revealTile(0, 0);
+    tile.getAttribute('value').then(function (arg) {
+      var val = parseInt(arg, 10);
+      expect(isNaN(val)).toBe(false);
+      expect(val).not.toEqual(-1);
+      expect(val).not.toEqual(-2);
+    });
+  });
+
+  it('should mark a tile', function () {
+    page.get();
+    page.setParams(9, 9, 10);
+    var grid = page.generateGrid();
+    var tile = grid.markTile(0, 0);
+    expect(tile.getAttribute('value')).toEqual('-2');
+  });
+
+
 });
